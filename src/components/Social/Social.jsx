ed800c526aa0a6694ecd25bf28afd052d11ca406
Julia_Johnson <julialj95@gmail.com>
2023-06-05 21:51:14 -0500

    add images

---
+++ src/components/Social/Social.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {FaLinkedin} from 'react-icons/fa';
+import {FaGithubSquare} from 'react-icons/fa';
+
+const Social = () => {
+  return (
+    <div>
+        <a href='https://www.linkedin.com/in/julialouisejohnson95/' target='_blank'><FaLinkedin/></a>
+        <a href="https://github.com/julialj95" target='_blank'><FaGithubSquare /></a>
+    </div>
+  )
+}
+
+export default Social
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.css';
 import Subheader from '../subheader/Subheader';
+import headshot from '../../images/IMG-5070.jpg';
+import Social from '../Social/Social';
 
 const Header = () => {
   return (
@@ -8,9 +10,14 @@ const Header = () => {
         <div className="container header__container">
             <h5>Welcome! I am</h5>
             <h1>Julia Johnson</h1>   
-            <h5>Fullstack Developer</h5>     
+            <h5>Fullstack Developer</h5>   
+            <Subheader />  
+            <Social />
+
+            <div>
+                <img src={headshot} alt="headshot"/>
+            </div>
         </div>
-        <Subheader />
     </header>
   )
 }
